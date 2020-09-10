35a35106d4c44ec45fce805a9fa19fc7570e648f
diegogv22 <diego.gv2209@gmail.com>
2020-09-10 12:06:17 -0500

    se modifica el java

--- prueba.java
+++ prueba.java
@@ -1,5 +1,13 @@
-public static void main (String [] arg){
-    System.out.println("Hola mundo");
-    //prueba dos
-
+public class vehiculos{
+    String color;
+    String marca;
+    int km;
+    public static void main (String [] arg){
+        System.out.println("Hola mundo");
+        //prueba dos
+        vehiculos auto1= new vehiculos();
+        auto1.color = "Rojo" //asdfasdfsdfasd
+        //display
+        System.out.println ("El color es: "+auto1.color);
+    }
 }
